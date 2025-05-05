8bba4442eb7e575e8eea3cf137886f878c3e01bc
carlost973 <153428731+carlost973@users.noreply.github.com>
2025-05-04 22:45:27 -0700

    Hour 11 Complete

--- ctaylor_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ ctaylor_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+    public float spacing = 1f;
     void Start()
     {
-        
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 spawnPosition = transform.position + new Vector3(i * spacing, 0, 0);
+            Instantiate(prefab, spawnPosition, transform.rotation);
+        }
     }
 
    
