51aebfb3910ebf8befebe72f9cdca087af0893dd
carlost973 <153428731+carlost973@users.noreply.github.com>
2025-05-11 01:48:36 -0700

    Hour 14 and 16 complete

---
+++ ctaylor_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,7 @@
+/*
+1. Added Collosion with collsion force of 70
+2.Added color over lifetime change(gradient) staring from red to green
+3.Changed the shape of the particle explosion to a hemisphere instead of a cone
+4.Added a Particle systems curve to change start size
+5.Changed rate over time (emission) to 100 
+*/
---
+++ ctaylor_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,4 @@
+/*
+ The modifcations I made were changing the background UI image to a more call of duty black ops look with the world map,
+added a couple more buttons for cosmetic purposes, and changed the game screen to a joke image.*/
+
