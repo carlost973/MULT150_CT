9db376c1acab531e6c82c2a3f43eba808bdf3184
carlost973 <153428731+carlost973@users.noreply.github.com>
2025-05-28 20:57:22 -0700

    Final Complete

---
+++ ctaylor_Final/Assets/Modifications.cs
@@ -0,0 +1,8 @@
+/*
+ 1. Directional light chnaged to a light faded red to make the laser walls look like they emit light to the ground
+2. Added semi dense fog to obscure where obstacles come from
+3. Chnaged skybox to a light reddish pink hue with dark red ground
+4. From orginal red cube to buzzsaw with red spikes, also added a red particle system with red trails so player can see in the fog.
+5.Made power up look sort of like a molecule with two mini primary colors spheres connected to one secondary color main sphere.
+6.Changed wall to prefab and made it two seperate colors by adding two walls together in the prefab.
+*/
